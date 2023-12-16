8bb2d90b2c218118e68e000da66b772c0102a594
PiyushMishra007 <piyushmishra.sushma@gmail.com>
2023-12-17 00:08:32 +0530

    server 1

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -7,6 +7,7 @@ export const addUser = async (request, response) => {
       response.status(200).json({ msg: "user already exist" });
       return;
     }
+
     const newUser = new User(request.body);
     await newUser.save();
     return response.status(200).json(newUser);
@@ -19,7 +20,8 @@ export const getUsers = async (request, response) => {
   try {
     const users = await User.find({});
     return response.status(200).json(users);
-  } catch (error) {
+  } 
+  catch (error) {
     return response.status(500).json(error.message);
   }
 };
